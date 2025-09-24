676bd1cc0bc61d2e51b286c5c2030d6d0587dc75
ipap0 <alkaz06@yandex.ru>
2025-09-24 16:16:47 +0300

    stage 1

--- min.py
+++ min.py
@@ -2,7 +2,11 @@ a = int(input('введите 1 число'))
 b = int(input('введите 2 число'))
 c = int(input('введите 3 число'))
 
-m =9999
+if a <= b and a <= c:
+    m = a
+elif b <= a and b <= c:
+    m = b
+else:
+    m = c
 
 print(f'минимум равен {m}')
-
