2342e6ab4873bd0fcaa948b94a9ccc1885943721
ipap0 <alkaz06@yandex.ru>
2025-09-25 11:24:06 +0300

    4 variants of min

--- min.py
+++ min.py
@@ -10,3 +10,31 @@ else:
     m = c
 
 print(f'минимум равен {m}')
+
+if a <= b and a <= c:
+    m = a
+if b <= a and b <= c:
+    m = b
+if c <= a and c <= b:
+    m = c
+
+print(f'минимум равен {m}')
+
+if a <= b:
+    if a <= c:
+        m = a
+    else:
+        m = c
+else:
+    if b <= c:
+        m = b
+    else:
+        m = c
+print(f'минимум равен {m}')
+
+m = a
+if b < m:
+    m = b
+if c < m:
+    m = c
+print(f'минимум равен {m}')
